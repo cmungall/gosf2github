a7b61f127b8eb4ffd412fb98a2ef4b9f518ca0ac
Chris Mungall <cjm@berkeleybop.org>
2015-06-18 12:06:43 -0700

    more cmd line documentation

--- gosf2github.pl
+++ gosf2github.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl -w
 use strict;
 use JSON;
-# see: 
 
 my $json = new JSON;
 
@@ -11,6 +10,10 @@ my $dry_run=0;
 my @collabs = ();
 my $default_assignee = 'cmungall';
 my $usermap = {};
+my $sf_base_url = "https://sourceforge.net/p/";
+my $sf_tracker = "";  ## e.g. obo/mouse-anatomy-requests
+my @default_labels = ();
+my $genpurls;
 while ($ARGV[0] =~ /^\-/) {
     my $opt = shift @ARGV;
     if ($opt eq '-h' || $opt eq '--help') {
@@ -26,8 +29,17 @@ while ($ARGV[0] =~ /^\-/) {
     elsif ($opt eq '-a' || $opt eq '--assignee') {
         $default_assignee = shift @ARGV;
     }
+    elsif ($opt eq '-s' || $opt eq '--sf-tracker') {
+        $sf_tracker = shift @ARGV;
+    }
+    elsif ($opt eq '-l' || $opt eq '--label') {
+        push(@default_labels, shift @ARGV);
+    }
     elsif ($opt eq '-k' || $opt eq '--dry-run') {
         $dry_run = 1;
+    }
+    elsif ($opt eq '--generate-purls') {
+        $genpurls = 1;
     }
     elsif ($opt eq '-c' || $opt eq '--collaborators') {
         @collabs = @{parse_json_file(shift @ARGV)};
@@ -60,9 +72,9 @@ foreach my $ticket (@tickets) {
     my $custom = $ticket->{custom_fields} || {};
     my $milestone = $custom->{_milestone};
 
-    my @labels = @{$ticket->{labels}};
+    my @labels = (@default_labels,  @{$ticket->{labels}});
 
-    push(@labels, "auto-migrated", map_priority($custom->{_priority}));
+    push(@labels, "sourceforge", "auto-migrated", map_priority($custom->{_priority}));
     if ($milestone) {
         push(@labels, $milestone);
     }
@@ -73,10 +85,36 @@ foreach my $ticket (@tickets) {
         $assignee = $default_assignee;
     }
 
+    my $body = $ticket->{description};
+
+    # fix SF-specific markdown
+    $body =~ s/\~\~\~\~/```/g;
+
+    if ($genpurls) {
+        my @lines = split(/\n/,$body);
+        foreach (@lines) {
+            last if m@```@;
+            next if m@^\s\s\s\s@;
+            s@(\S+):(\d+)@[$1:$2](http://purl.obolibrary.org/obo/$1_$2)@g;
+        }
+        $body = join("\n", @lines);
+    }
+
+    # it is tempting to prefix with '@' but this may generate spam and get the bot banned
+    $body .= "\n\nOriginal comment by: \@".map_user($ticket->{reported_by});
+    $body .= "\n\nOriginal comment by: ".map_user($ticket->{reported_by});
+
+    my $num = $ticket->{ticket_num};
+    if ($sf_tracker) {
+        my $turl = "$sf_base_url$sf_tracker/$num";
+        ##$body .= "\n\nOriginal Ticket: [$sf_tracker/$num]($turl)";
+        $body .= "\n\nOriginal Ticket: $turl";
+    }
+
     my $issue =
     {
         "title" => $ticket->{summary},
-        "body" => $ticket->{description}."\n\nOriginal comment by: ".map_user($ticket->{reported_by}),
+        "body" => $body,
         "created_at" => cvt_time($ticket->{created_date}),    ## check
         "assignee" => $assignee,
         #"milestone" => 1,  # todo
@@ -168,13 +206,15 @@ sub usage {
     my $sn = scriptname();
 
     <<EOM;
-$sn [-h] [-u USERMAP] [-c COLLABINFO] [-r REPO] [-t OATH_TOKEN] [-a USERNAME] [--dry-run] TICKETS-JSON-FILE
+$sn [-h] [-u USERMAP] [-c COLLABINFO] [-r REPO] [-t OATH_TOKEN] [-a USERNAME] [-l LABEL]* [-s SF_TRACKER] [--dry-run] TICKETS-JSON-FILE
 
 Migrates tickets from sourceforge to github, using new v3 GH API, documented here: https://gist.github.com/jonmagic/5282384165e0f86ef105
 
 This assumes that you have exported your tickets from SF. E.g. from a page like this: https://sourceforge.net/p/obo/admin/export
 
-    
+It also assumes you have create an OAuth token:
+
+https://github.com/settings/tokens    
 
 Example:
 
@@ -185,11 +225,19 @@ gosf2github.pl -u users_sf2gh.json -c plant-eo-test-collab.json -r cmungall/plan
 
 ARGUMENTS:
 
-   -r | --repo   REPO
-                 E.g. cmungall/sf-test
+   -k | --dry-run
+                 Do not execute github API calls; print curl statements instead
 
-   -t | --token  TOKEN 
+   -r | --repo   REPO
+                 Examples: cmungall/sf-test, obophenotype/cell-ontology
+
+   -t | --token  TOKEN
                  OATH token. Get one here: https://github.com/settings/tokens
+                 Note that all tickets and issues will appear to originate from the user that generates the token
+
+   -l | --label  LABEL
+                 Add this label to all tickets, in addition to defaults and auto-added.
+                 Currently the following labels are ALWAYS added: auto-migrated, a priority label (unless priority=5), a label for every SF label, a label for the milestone
 
    -u | --usermap USERMAP-JSON-FILE
                   Maps SF usernames to GH
@@ -203,6 +251,15 @@ ARGUMENTS:
                   Generate like this:
                   curl -H "Authorization: token TOKEN  https://api.github.com/repos/cmungall/sf-test/collaborators > sf-test-collab.json
 
+   -s | --sf-tracker  NAME
+                 E.g. obo/mouse-anatomy-requests
+                 If specified, will append the original URL to the body of the new issue. E.g. https://sourceforge.net/p/obo/mouse-anatomy-requests/90
+
+NOTES:
+
+ * uses a pre-release API documented here: https://gist.github.com/jonmagic/5282384165e0f86ef105
+ * milestones are converted to labels
+ * all issues and comments will appear to have originated from the user who issues the OAth ticket
 
 TIP:
 
